173ed16e458c89dbab000f7e8eb7c42e64882a71
Alex <37003319+alchess@users.noreply.github.com>
2018-03-03 19:17:43 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,3 @@
-﻿using System;
 
 namespace ConsoleApp1
 {
@@ -7,46 +6,63 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //some global var's//
-           /////////////////////
+            /////////////////////
             int j = 0;
-            int ff = 100;
-            int[] answer = new int[10];
-            string iString;
-            
-            
-            while (ff < 9999)
-            {
-              
-                iString = Convert.ToString(ff);
-              
-                //separation of number for make units//
-                int n1 = Convert.ToInt32(Convert.ToString(iString[0]));
-                int n2 = Convert.ToInt32(Convert.ToString(iString[1]));
-                int n3 = Convert.ToInt32(Convert.ToString(iString[2]));
-                
-                //factorial calculation//
-                int fn1 = Factorial(n1);
-                int fn2 = Factorial(n2);
-                int fn3 = Factorial(n3);
-                //check on right execution and if all ok add this number in array
-                if (fn1+fn2+fn3 == ff) { if (answer[j] == 0) { answer[j++] = ff; } } 
 
-                //function to calculate factorials
-                int Factorial(int numb)
+            int[] answer = new int[5];
+            string iString;
+            int n1 = 0;
+            int n2 = 0;
+            int n3 = 0;
+            int n4 = 0;
+
+            int ff = 0;
+            int maxNum = 9999;
+
+            int Factorial(int numb)
                 {
                     int res = 1;
                     for (int i = numb; i > 1; i--)
                         res *= i;
                     return res;
                 }
-                ff++;
-             }
-            //displaying items of array
-            for(int ii=0; ii < 5; ii++) { Console.WriteLine(answer[ii]);}
-            
-            //console pause
-            Console.ReadKey();   
-        }
 
+            while (ff <= maxNum)
+            {
+                iString = Convert.ToString(ff);
+
+                n1 = Factorial(Convert.ToInt32(Convert.ToString(iString[0])));
+                if (ff >= 10)
+                {
+                    n2 = Factorial(Convert.ToInt32(Convert.ToString(iString[1])));
+                    if (ff >= 100)
+                    {
+                        n3 = Factorial(Convert.ToInt32(Convert.ToString(iString[2])));
+                        if (ff >= 1000)
+                        {
+                            n4 = Factorial(Convert.ToInt32(Convert.ToString(iString[3])));
+                        }
+                    }
+                }
+
+
+                //check on right execution and if all ok add this number in array               
+                if (ff == 0) { answer[0] = 1; j++; }
+                if (n1 + n2 + n3  == ff) {answer[j++] = ff;}
+
+
+                //function to calculate factorials
+
+                ff++;
+            }
+
+            foreach (int qi in answer)
+            {
+                System.Console.WriteLine("{0}", qi);
+            }
+
+            //console pause
+            Console.ReadKey();
+        }
     }
 }
