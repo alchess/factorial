3ecdaeb38676eeb840d791461da74b57a2e72501
alchess <37003319+alchess@users.noreply.github.com>
2018-03-03 03:03:56 +0300

    Update Program.cs
    Added comments and some corrections

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,25 +6,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //some global var's//
+           /////////////////////
             int j = 0;
             int ff = 100;
             int[] answer = new int[10];
-
+            string iString;
+            
+            
             while (ff < 9999)
             {
-                string iString;
+              
                 iString = Convert.ToString(ff);
-
+              
+                //separation of number for make units//
                 int n1 = Convert.ToInt32(Convert.ToString(iString[0]));
                 int n2 = Convert.ToInt32(Convert.ToString(iString[1]));
                 int n3 = Convert.ToInt32(Convert.ToString(iString[2]));
-
+                
+                //factorial calculation//
                 int fn1 = Factorial(n1);
                 int fn2 = Factorial(n2);
                 int fn3 = Factorial(n3);
-
+                //check on right execution and if all ok add this number in array
                 if (fn1+fn2+fn3 == ff) { if (answer[j] == 0) { answer[j++] = ff; } } 
 
+                //function to calculate factorials
                 int Factorial(int numb)
                 {
                     int res = 1;
@@ -34,8 +41,10 @@ namespace ConsoleApp1
                 }
                 ff++;
              }
+            //displaying items of array
             for(int ii=0; ii < 5; ii++) { Console.WriteLine(answer[ii]);}
             
+            //console pause
             Console.ReadKey();   
         }
 
